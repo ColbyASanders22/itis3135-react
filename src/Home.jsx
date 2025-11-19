97a596d43ea7c2af7a38d072f30cd9cebd74ae03
Colby <csande53@uncc.edu>
2025-11-19 11:47:15 -0500

    fixes

---
+++ src/Home.jsx
@@ -0,0 +1,15 @@
+import Header from './Header';
+
+export default function Home() {
+    document.title += "Colby Sanders Clever Salmon || Introduction";
+  return (
+    <>
+           <Header></Header>
+    <h2>Welcome!</h2>
+	<div class="text-box">
+    <p>This is all the content for my ITIS 3135 class</p>
+	</div>
+      
+    </>
+  );
+}

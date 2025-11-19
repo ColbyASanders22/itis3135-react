13c48d2146df03cb42be9ecfbca50b9bf77132e4
Colby <csande53@uncc.edu>
2025-11-19 12:09:11 -0500

    testing

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,12 @@
 import Header from './Header';
+import "./home.css";
 
 export default function Home() {
     document.title += "Colby Sanders Clever Salmon || Home";
   return (
     <>
     <h2>Welcome!</h2>
-	<div class="text-box">
+	<div className="text-box">
     <p>This is all the content for my ITIS 3135 class</p>
 	</div>
       
